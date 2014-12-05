92275b94f37ad0f584c788dea5a8365cd979ac32
Satbir Saini <satbir.kira@gmail.com>
2014-12-04 19:21:32 -0500

    old type

--- C/mallocandfree.c
+++ C/mallocandfree.c
@@ -4,20 +4,20 @@
 
 int main (void)
 {
-   //Declare a pointer to hold an adress
+   //Declare a pointer to hold an address
    int *ptr;
    
    //Request address where there's room to store an int
-   //as a pointer. It poist to open space in memory.
+   //as a pointer. It points to open space in memory.
    ptr = malloc(sizeof(int));
    
    if (ptr == NULL)
    {  
-      printf("Didn't get requested memort");
+      printf("Didn't get requested memory");
       exit(-1);   
    }
    
-   //Do something with memort
+   //Do something with memory
    *ptr = 7;
    printf("The value of my pointer is: %p\n", ptr);
    printf("The value pointed to is: %d\n", *ptr);
